5d76b142f2e430fc4f245c5a4724ffdeba6861ab
ChangEn Yea <nsce9806q@gmail.com>
2025-02-20 18:00:01 +0900

    First commit

---
+++ src/main/java/com/cloudy/api/TestController.java
@@ -0,0 +1,36 @@
+package com.cloudy.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriBuilder;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/health")
+    public ResponseEntity<?> healthCheck() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/circle")
+    public ResponseEntity<?> getCircle(@RequestParam("x") String x, @RequestParam("y") String y) {
+        var restClient = RestClient.builder().baseUrl("http://jeju.mms.gislab.co.kr/mms_new/").build();
+        return restClient.get().uri(uriBuilder -> uriBuilder.path("GEONET.getTimePopByCircle.php")
+                .queryParam("SELECT", "M_POP_00,M_POP_10,M_POP_20,M_POP_30,M_POP_40,M_POP_50,M_POP_60,M_POP_70,M_POP_80,M_POP_90,W_POP_00,W_POP_10,W_POP_20,W_POP_30,W_POP_40,W_POP_50,W_POP_60,W_POP_70,W_POP_80,W_POP_90")
+                .queryParam("X", x)
+                .queryParam("Y", y)
+                .queryParam("R", "150")
+                .build()
+        ).retrieve().toEntity(String.class);
+
+//        http://jeju.mms.gislab.co.kr/mms_new/GEONET.getTimePopByCircle.php?SELECT=M_POP_00,M_POP_10,M_POP_20,M_POP_30,M_POP_40,M_POP_50,M_POP_60,M_POP_70,M_POP_80,M_POP_90,W_POP_00,W_POP_10,W_POP_20,W_POP_30,W_POP_40,W_POP_50,W_POP_60,W_POP_70,W_POP_80,W_POP_90&X=&Y=33.459134970543&R=150
+
+    }
+}
